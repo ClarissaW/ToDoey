ba28b9f744251abfc223aa673946009de1c8cbed
Zewen Wang <w.zewen@outlook.com>
2018-01-19 10:31:21 -0500

    Data persisted using UserDefaults method

--- ToDoey/TodoListViewController.swift
+++ ToDoey/TodoListViewController.swift
@@ -11,8 +11,13 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggs", "Destory Demogorgon"]
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+            itemArray = items
+        }
+//        itemArray = defaults.array(forKey: "ToDoListArray") as! [String] // will crash when ToDoListArry does not exist
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -41,6 +46,8 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new ToDoey item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
 //            //self.itemArray.append(textField.text ?? "New Item") // if the user did not input text in this field, it will automatically add "New Item" in the tableview
             
